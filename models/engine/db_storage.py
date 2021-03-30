aa90f5e8caee6858d6822723cb0c57ae54cbe6e5
aureliewouy <aureliecedia@yahoo.com>
2021-03-30 21:17:16 +0000

    update user, place and city table

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -6,6 +6,8 @@ from models.base_model import Base
 from models.base_model import BaseModel
 from models.city import City
 from models.state import State
+from models.user import User
+from models.place import Place
 from os import getenv
 
 
