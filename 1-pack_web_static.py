a3dca4b243f667df7d1a69fc69dfdb8d37082853
aureliewouy <aureliecedia@yahoo.com>
2021-04-15 15:46:58 +0000

    update parameters

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
 """ Fabric script that generates from a content into a .tgz archive """
-from fabric.api import local
-from time import strftime
+from fabric.api import *
+from datetime import datetime
+import os.path
+env.hosts = ['34.75.49.102', '35.196.238.0']
+env.user = 'ubuntu'
+env.password = '~/.ssh/id_rsa'
 
 
 def do_pack():
     """
     Generates a .tgz archive
     """
-    local("mkdir -p versions")
-    timef = strftime("%Y%m%d%H%M%S")
-    pack = local("tar -cvzf versions/web_static_{}.tgz web_static/"
-                 .format(timef))
+    if not os.path.isdir("versions"):
+        local("mkdir -p versions")
+    timef = datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")
+    filename = "versions/web_static_{}.tgz".format(timef)
+    pack = local("tar -cvzf {} web_static"
+                 .format(filename))
     if pack.failed:
         return None
-    return pack
+    return filename
